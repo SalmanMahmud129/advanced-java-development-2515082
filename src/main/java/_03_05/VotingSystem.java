57b177a9bc2fcfd127ba292aefe182aca115a10f
Salman Mahmud <salman_mahmud@hotmail.com>
2023-08-01 15:36:52 -0400

    Advanced data structures

--- src/main/java/_03_05/VotingSystem.java
+++ src/main/java/_03_05/VotingSystem.java
@@ -6,16 +6,18 @@ public class VotingSystem {
 
     // Create a LinkedHashMap here, with the contestant name as the key
     // and the number of votes as the value.
+    LinkedHashMap<String, Integer> contestantVotes = new LinkedHashMap<>();
 
     void vote(String contestant) {
         // Implement this method so that a new contestant is added to the map if it's
         // not already there. If it is already there, increment the value by one.
+        contestantVotes.merge(contestant, 1, Integer::sum);
     }
 
     // This method should return the LinkedHashMap field you create at the top of
     // the class.
     public LinkedHashMap getVotes() {
-        return new LinkedHashMap();
+        return contestantVotes;
     }
 
 
