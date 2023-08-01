20ce0daf373b0aad7abd1a1f8009d3ee17c9c65c
Salman Mahmud <salman_mahmud@hotmail.com>
2023-07-31 23:15:35 -0400

    Generics

--- src/main/java/_02_04/before/ClothingSite.java
+++ src/main/java/_02_04/before/ClothingSite.java
@@ -1,9 +1,38 @@
 package _02_04.before;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClothingSite {
+
+    public static void main(String[] args){
+        ShirtItem shirtItem = new ShirtItem();
+
+        checkoutItem(shirtItem);
+
+        JacketItem jacket = new JacketItem();
+        
+
+        checkoutItem(jacket);
+        
+
+        List<ClothingItem> clothingItems = new ArrayList<>();
+        clothingItems.add(shirtItem);
+        clothingItems.add(jacket);
+
+        checkoutAll(clothingItems);
+    }
 
     static void checkoutItem(ClothingItem item) {
         System.out.println("Item purchased: " + item.getName() + ", price: " + item.getPrice());
+    }
+
+    static void checkoutAll(List<ClothingItem> clothingItems){
+
+        for(ClothingItem item : clothingItems){
+            checkoutItem(item);
+        }
+
     }
 
 
